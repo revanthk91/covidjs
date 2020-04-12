8a0b1646933b2a723ebcf54fb5c87dd5c2ff8e72
Revanth Kumar <gamingkid45@gmail.com>
2020-04-12 20:03:24 +0530

    Finall COMMIT

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const port = process.env.PORT || 5000
+const port = 443
 const axios = require('axios')
 const cheerio = require('cheerio')
 const siteUrl = 'https://www.worldometers.info/coronavirus/country/india/'
@@ -7,9 +7,10 @@ const fs = require('fs')
 const ejs = require('ejs')
 const cron = require('node-cron')
 const http = require('http')
+const moment = require('moment-timezone')
 
 const appUrl = "http://covid-india-69.herokuapp.com"
-
+const apiUrl = "https://api.data.gov.in/resource/cd08e47b-bd70-4efb-8ebc-589344934531?format=viz&limit=all&api-key=579b464db66ec23bdd000001cdc3b564546246a772a26393094f5645&_=1586501432931"
 
 const server = express()
 server.listen(port, () => {
@@ -18,6 +19,19 @@ server.listen(port, () => {
 
 
 //Kernal Functions
+const currentTime = () => { return moment().tz("Asia/Kolkata").format('MMMM Do YYYY, h:mm:ss a') }
+
+const updateState = async (url) => {
+	let stateData = await axios.get(url)
+	let coreData = JSON.parse(fs.readFileSync('./core.json'))
+	stateData = stateData.data.data
+	stateData.sort((a,b) => (a[2]-b[2])*-1) //sort
+
+	coreData.states = stateData
+	fs.writeFileSync('./core.json',JSON.stringify(coreData),'utf-8')//statesSave
+}
+
+
 const update = async (url) => {
 	//collection
 	const html = await axios.get(url)
@@ -39,7 +53,7 @@ const cachePage = (newData) => {
 
 	var coreData = JSON.parse(fs.readFileSync('./core.json'))
 	coreData.total = newData
-	coreData.lastUpdate = new Date() //timeUpdate
+	coreData.lastUpdate = currentTime() //timeUpdate
 
 	fs.writeFileSync("./core.json",JSON.stringify(coreData),'utf-8') //coreUpdate 
 
@@ -63,29 +77,34 @@ const commaProcess = (stringNums) => {
 }
 
 
+
 //Scheduled Cron Jobs
 
 cron.schedule("0 0 */1 * * *", () => {
 	update(siteUrl)
-	console.log('hourly updated cachce at ' + new Date())
+	console.log('hourly updated cachce at ' + currentTime())
 })
 
-cron.schedule("0 59 23 * * *", () => {
+cron.schedule("0 30 18 * * *", () => {
+	console.log('midnight m812 update. States and Day Count Updated.')
 	let coreData = JSON.parse(fs.readFileSync('./core.json'))
 	coreData.lastDay = coreData.total; //reseting last day to end Day count
 	fs.writeFileSync('./core.json',JSON.stringify(coreData),'utf-8')
+
+	updateState(apiUrl) //statesUpdate
 })
 
-//aright bois. we got a Old Linux 8gb SSD Laptop with 24/7 power and network.
+update(siteUrl)
 
 ///. basic static serve.
 server.use(express.static('public'))
-
 
 /*
 
 1. fetch and save on hardisk (every hour)
 2. static serve (index.htm,custom.css)
+
+//aright bois. we got a Old Linux 8gb SSD Laptop with 24/7 power and network.
 */
 
 
